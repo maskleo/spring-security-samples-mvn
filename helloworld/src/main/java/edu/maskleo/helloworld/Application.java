6507b0eddae1dcfd2d15dc15ef1b4db7700ec260
maskleo <masque@aliyun.com>
2018-05-24 15:35:13 +0800

    添加 spring-boot-starter-security 依赖

--- helloworld/src/main/java/edu/maskleo/helloworld/Application.java
+++ helloworld/src/main/java/edu/maskleo/helloworld/Application.java
@@ -1,9 +1,13 @@
 package edu.maskleo.helloworld;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 public class Application {
 
     public static void main(String[] args) {
